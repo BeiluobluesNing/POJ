cd5964ff71dda1d8524f886031815c9bf9a86389
BeiluobluesNing <529599524@qq.com>
2018-04-08 21:18:24 +0800

    Update poj_2178.cpp

--- poj_2178.cpp
+++ poj_2178.cpp
@@ -33,54 +33,54 @@ void mom_turn(int prev_t, int prev_hp, int prev_pos,int h_hp, int pos, int mom_h
                                           make_pair(magic,magic_param));
 }
 
-//int main(void){
-//    cin >> N >> HP_H >> MP_H >> HP_M >> N_M >> V >> dH;
-//    for(int i=0;i<N;i++)
-//        cin >> damages[i];
-//    memset(tbl, -1, sizeof(tbl));           //初始化，其余情况为-1
-//    tbl[0][HP_H][N-1] = HP_M * N_M;         //回合数 怪兽最初位置 英雄血量 英雄 怪兽此时血量
-//    int ans_turn = -1, ans_hp = 0, ans_pos = 0;
-//    for(int t=0;t<MP_H; t++){
-//        for(int hp = 1; hp <= HP_H; hp++){
-//            for(int pos =0; pos<N;pos++){
-//                if(tbl[t][hp][pos] == -1) continue;
-//                int mom_hp = tbl[t][hp][pos];
-//                if(mom_hp <= damages[pos]){
-//                    // do_lightning
-//                    ans_turn = t;
-//                    ans_hp   = hp;
-//                    ans_pos  = pos;
-//                    break;
-//                }
-//                mom_turn(t, hp, pos, hp, pos, mom_hp-damages[pos], 'L', 0);
-//                mom_turn(t, hp, pos, min(HP_H,hp+dH), pos, mom_hp, 'H', 0);
-//                for(int i=0;i<N;i++)
-//                    mom_turn(t, hp, pos, hp, i, mom_hp, 'T', i+1);
-//            }
-//            if(ans_turn != -1)
-//                break;
-//        }
-//        if(ans_turn != -1)
-//            break;
-//    }
-//    if(ans_turn == -1){
-//        cout << "DEFEATED" << endl;
-//    }else{
-//        cout << "VICTORIOUS" << endl;
-//        vector<pair<char,int> > v;
-//        while(ans_turn != 0){
-//            pair<pair<int,int>,pair<char,int> > info = back[ans_turn][ans_hp][ans_pos];
-//            v.push_back(info.second);
-//            ans_turn--;
-//            ans_hp  = info.first.first;
-//            ans_pos = info.first.second;
-//        }
-//        for(int i=(int)v.size()-1; i>=0;i--){
-//            if(v[i].first == 'T')
-//                cout << "T " << v[i].second << endl;
-//            else 
-//                cout << v[i].first << endl;
-//        }
-//        cout << "L" << endl;
-//    }
-//}
+int main(void){
+   cin >> N >> HP_H >> MP_H >> HP_M >> N_M >> V >> dH;
+   for(int i=0;i<N;i++)
+       cin >> damages[i];
+   memset(tbl, -1, sizeof(tbl));           //初始化，其余情况为-1
+   tbl[0][HP_H][N-1] = HP_M * N_M;         //回合数 怪兽最初位置 英雄血量 英雄 怪兽此时血量
+   int ans_turn = -1, ans_hp = 0, ans_pos = 0;
+   for(int t=0;t<MP_H; t++){
+       for(int hp = 1; hp <= HP_H; hp++){
+           for(int pos =0; pos<N;pos++){
+               if(tbl[t][hp][pos] == -1) continue;
+               int mom_hp = tbl[t][hp][pos];
+               if(mom_hp <= damages[pos]){
+                   // do_lightning
+                   ans_turn = t;
+                   ans_hp   = hp;
+                   ans_pos  = pos;
+                   break;
+               }
+               mom_turn(t, hp, pos, hp, pos, mom_hp-damages[pos], 'L', 0);
+               mom_turn(t, hp, pos, min(HP_H,hp+dH), pos, mom_hp, 'H', 0);
+               for(int i=0;i<N;i++)
+                   mom_turn(t, hp, pos, hp, i, mom_hp, 'T', i+1);
+           }
+           if(ans_turn != -1)
+               break;
+       }
+       if(ans_turn != -1)
+           break;
+   }
+   if(ans_turn == -1){
+       cout << "DEFEATED" << endl;
+   }else{
+       cout << "VICTORIOUS" << endl;
+       vector<pair<char,int> > v;
+       while(ans_turn != 0){
+           pair<pair<int,int>,pair<char,int> > info = back[ans_turn][ans_hp][ans_pos];
+           v.push_back(info.second);
+           ans_turn--;
+           ans_hp  = info.first.first;
+           ans_pos = info.first.second;
+       }
+       for(int i=(int)v.size()-1; i>=0;i--){
+           if(v[i].first == 'T')
+               cout << "T " << v[i].second << endl;
+           else 
+               cout << v[i].first << endl;
+       }
+       cout << "L" << endl;
+   }
+}
